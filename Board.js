def661c2060a08ab442ef4d0436fe5a800ec2cf4
Miike Ramos <mikeramos@gmail.com>
2013-07-01 00:24:31 -0700

    N-Queens The Night Away

--- Board.js
+++ Board.js
@@ -1,8 +1,8 @@
-(function(){
+(function () {
 
   window.Board = Backbone.Model.extend({
 
-    initialize: function(params){
+    initialize: function (params) {
       if (params.n) {
         this.set(makeEmptyMatrix(this.get('n')));
       } else {
@@ -10,90 +10,203 @@
       }
     },
 
-    rows: function(){
-      return _(_.range(this.get('n'))).map(function(rowIndex){
+    rows: function () {
+      return _(_.range(this.get('n'))).map(function (rowIndex) {
         return this.get(rowIndex);
       }, this);
     },
 
-    togglePiece: function(rowIndex, colIndex){
-      this.get(rowIndex)[colIndex] = + !this.get(rowIndex)[colIndex];
+    getColumn: function (colIndex) {
+      return _(_.range(this.get('n')))
+        .map(function (rowIndex) {
+        return this.get(rowIndex)[colIndex];
+      }, this);
+    },
+
+    togglePiece: function (rowIndex, colIndex) {
+      // console.log(this.get(rowIndex)[colIndex]);
+
+      this.get(rowIndex)[colIndex] = +!this.get(rowIndex)[colIndex];
       this.trigger('change');
     },
 
-    _getFirstRowColumnIndexForMajorDiagonalOn: function(rowIndex, colIndex){
+    _getFirstRowColumnIndexForMajorDiagonalOn: function (rowIndex, colIndex) {
       return colIndex - rowIndex;
     },
 
-    _getFirstRowColumnIndexForMinorDiagonalOn: function(rowIndex, colIndex){
+    _getFirstRowColumnIndexForMinorDiagonalOn: function (rowIndex, colIndex) {
       return colIndex + rowIndex;
     },
 
-
-    hasAnyRooksConflicts: function(){
+    hasAnyRooksConflicts: function () {
       return this.hasAnyRowConflicts() || this.hasAnyColConflicts();
     },
 
-    hasAnyQueenConflictsOn: function(rowIndex, colIndex){
+    hasAnyQueenConflictsOn: function (rowIndex, colIndex) {
       return (
         this.hasRowConflictAt(rowIndex) ||
         this.hasColConflictAt(colIndex) ||
         this.hasMajorDiagonalConflictAt(this._getFirstRowColumnIndexForMajorDiagonalOn(rowIndex, colIndex)) ||
-        this.hasMinorDiagonalConflictAt(this._getFirstRowColumnIndexForMinorDiagonalOn(rowIndex, colIndex))
-      );
+        this.hasMinorDiagonalConflictAt(this._getFirstRowColumnIndexForMinorDiagonalOn(rowIndex, colIndex)));
     },
 
-    hasAnyQueensConflicts: function(){
+    hasAnyQueensConflicts: function () {
       return this.hasAnyRooksConflicts() || this.hasAnyMajorDiagonalConflicts() || this.hasAnyMinorDiagonalConflicts();
     },
 
-    _isInBounds: function(rowIndex, colIndex){
+    _isInBounds: function (rowIndex, colIndex) {
       return (
         0 <= rowIndex && rowIndex < this.get('n') &&
-        0 <= colIndex && colIndex < this.get('n')
-      );
+        0 <= colIndex && colIndex < this.get('n'));
     },
-
 
     // todo: fill in all these functions - they'll help you!
 
-    hasRowConflictAt: function(rowIndex){
-      return false; // fixme
+    hasRowConflictAt: function (rowIndex) {
+      var result = false;
+      var row = this.get(rowIndex);
+      var rowSum = _(row).reduce(function (memo, num) {
+        return memo + num;
+      }, 0);
+      if (rowSum > 1) {
+        result = true;
+      }
+      return result;
     },
 
-    hasAnyRowConflicts: function(){
-      return false; // fixme
+    hasAnyRowConflicts: function () {
+      var result = false;
+      totalRows = this.get('n'); // --> 4
+
+      for (var i = 0; i < totalRows; i++) {
+        if (this.hasRowConflictAt(i)) {
+          result = true;
+        }
+      }
+      return result;
     },
 
-    hasColConflictAt: function(colIndex){
-      return false; // fixme
+    hasColConflictAt: function (colIndex) {
+      var result = false;
+      var col = this.getColumn(colIndex);
+      var colSum = _(col).reduce(function (memo, num) {
+        return memo + num;
+      }, 0);
+      if (colSum > 1) {
+        result = true;
+      }
+      return result;
     },
 
-    hasAnyColConflicts: function(){
-      return false; // fixme
+    hasAnyColConflicts: function () {
+      var result = false;
+      totalCols = this.get('n'); // --> 4
+
+      for (var i = 0; i < totalCols; i++) {
+        if (this.hasColConflictAt(i)) {
+          result = true;
+        }
+      }
+      return result;
     },
 
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow){
-      return false; // fixme
+    hasMajorDiagonalConflictAt: function (majorDiagonalColumnIndexAtFirstRow) {
+      // var n = this.get('n');
+      // var sum = 0;
+      // // debugger;
+      // for (var i = 0; i < n; i++) {
+      //   if (this.rows()[i][index + i]) {
+      //     sum++;
+      //   }
+      // }
+      // return !!(sum > 1);
+      var rows = this.rows();
+
+      var sum = 0;
+      var rowIndex = 0;
+
+      for(var i=majorDiagonalColumnIndexAtFirstRow; i<rows.length; i++) {
+        if((i>-1)&&(rowIndex<rows.length)) {
+          sum += rows[rowIndex][i];
+        }
+        rowIndex++;
+      }
+
+      return (sum>1) ? true : false;
     },
 
-    hasAnyMajorDiagonalConflicts: function(){
-      return false; // fixme
+    hasAnyMajorDiagonalConflicts: function () {
+      // var result = false;
+      // totalDiags = this.get('n'); // --> 4
+
+      // for (var i = 0; i < totalDiags; i++) {
+      //   if (this.hasMajorDiagonalConflictAt(i)) {
+      //     result = true;
+      //   }
+      // }
+
+      // return result;
+      var n = this.rows().length;
+      var start = (n-2)*-1;
+
+      for(var i=start; i<n-1; i++) {
+        if(this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow){
-      return false; // fixme
+    hasMinorDiagonalConflictAt: function (minorDiagonalColumnIndexAtFirstRow) {
+      // var n = this.get('n');
+      // var sum = 0;
+      // // debugger;
+      // for (var i = 0; i < n; i++) {
+      //   if (this.rows()[i][index - i]) {
+      //     sum++;
+      //   }
+      // }
+      // return !!(sum > 1);
+      var rows = this.rows();
+
+      var sum = 0;
+      var rowIndex = 0;
+
+      for(var i=minorDiagonalColumnIndexAtFirstRow; i>=0; i--) {
+        if((i<rows.length)&&(rowIndex<rows.length)) {
+          sum += rows[rowIndex][i];
+        }
+        rowIndex++;
+      }
+
+      return (sum>1) ? true : false;
     },
 
-    hasAnyMinorDiagonalConflicts: function(){
-      return false; // fixme
-    }
+    hasAnyMinorDiagonalConflicts: function () {
+      // var result = false;
+      // totalDiags = this.get('n'); // --> 4
+
+      // for (var i = 0; i < totalDiags; i++) {
+      //   if (this.hasMinorDiagonalConflictAt(i)) {
+      //     result = true;
+      //   }
+      // }
+
+      // return result;
+      var n = this.rows().length;
+      for(var i=1; i< n + (n-2); i++) {
+        if(this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
+    },
 
   });
 
-  var makeEmptyMatrix = function(n){
-    return _(_.range(n)).map(function(){
-      return _(_.range(n)).map(function(){
+  var makeEmptyMatrix = function (n) {
+    return _(_.range(n)).map(function () {
+      return _(_.range(n)).map(function () {
         return 0;
       });
     });
--- solvers.js
+++ solvers.js
@@ -2,41 +2,161 @@
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
-window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', solution);
-  return solution;
+var makeEmptyMatrix = function (n) {
+  return _(_.range(n)).map(function () {
+    return _(_.range(n)).map(function () {
+      return 0;
+    });
+  });
 };
 
-window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+window.findNRooksSolution = function (n) {
+  var board = new Board(makeEmptyMatrix(n));
+  var solution = false;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var iterate = function (row) {
+      for (var i = 0; i < n; i++) {
+        board.togglePiece(row, i);
+        if (row === n-1){
+          solution = true;
+          return;
+        }
+        if (board.hasAnyRooksConflicts) {
+          board.togglePiece(row, i);
+        } else {
+          return iterate(row + 1);
+        }
+      }
+    };
+    //if I successfully place a queen AND im in the last row, print my current board!
+
+ iterate(0);
+
+console.log('Single solution for ' + n + ' rooks:', board);
+return board;
 };
 
-window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+function factorial(n) {
+  if (n < 0) {
+    // Termination condition to prevent infinite recursion
+    console.log("ALERT: you entered a negative number which will break ths code");
+    return;
+  }
+  // Base case
+  if (n === 0) {
+    return 1;
+  }
+  // Recursive case
+  return n * factorial(n - 1);
+}
 
-  console.log('Single solution for ' + n + ' queens:', solution);
-  return solution;
+window.countNRooksSolutions = function (n) {
+  var solutionCount = function (n) {
+    if (n < 0) {
+      return;
+    }
+    // Base case
+    if (n === 0) {
+      return 1;
+    }
+    // Recursive case
+    return n * solutionCount(n - 1);
+  };
+  return solutionCount(n);
+};
+
+window.findNQueensSolution = function (n) {
+  var board = new Board(makeEmptyMatrix(n));
+  var solution = false;
+
+  var iterate = function (row) {
+      for (var i = 0; i < n; i++) {
+        board.togglePiece(row, i);
+        if (row === n-1){
+          solution = true;
+          return;
+        }
+        if (board.hasAnyQueensConflicts) {
+          board.togglePiece(row, i);
+        } else {
+          return iterate(row + 1);
+        }
+      }
+    };
+  iterate(0);
+
+  console.log('Single solution for ' + n + ' queens:', board);
+  return board;
 };
 
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  var matrix = new Board(makeEmptyMatrix(n));
+  var solutionCount = 0;
+  var queenCount = 0;
+  var row = 0;
+  var column = 0;
+  var completed = false;
 
+  var backToParent = function(){
+    row -= 1;
+    var currentRow = matrix.get(row);
+    var queenPosition = currentRow.indexOf(1);
+    matrix.togglePiece(row,queenPosition);
+    column = queenPosition+1;
+    queenCount -= 1;
+  };
+
+  var walkMatrix = function() {
+    if (column < n) {
+      if (queenCount === n){
+        solutionCount += 1;
+        backToParent();
+        walkMatrix();
+      }
+      if (row === -1) {
+        return;
+      }
+      matrix.togglePiece(row,column);
+      if (matrix.hasAnyQueensConflicts()) {
+        matrix.togglePiece(row,column);
+        column += 1;
+        if (column === n){
+          backToParent();
+          walkMatrix();
+        } else {
+          walkMatrix();
+        }
+      } else {
+          queenCount += 1;
+          row += 1;
+          column = 0;
+          walkMatrix();
+      }
+    } else {
+      if (row > 0) {
+        backToParent();
+      } else {
+        row -= 1;
+      }
+      if(row < 0) {
+        return solutionCount;
+      } else {
+        walkMatrix();
+      }
+    }
+  };
+
+  walkMatrix();
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
 
-
 // This function uses a board visualizer lets you view an interactive version of any piece matrix.
 
-window.displayBoard = function(matrix){
+window.displayBoard = function (matrix) {
   $('body').html(
     new BoardView({
-      model: new Board(matrix)
-    }).render()
-  );
+    model: new Board(matrix)
+  }).render());
 };
+
